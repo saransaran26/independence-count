efa016269ba4a934be7391fed2bf83f1519bfba3
saransaran26 <137371635+saransaran26@users.noreply.github.com>
2023-07-21 18:08:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+
+let n10 = document.getElementById("n10")
+let container = document.getElementById("main")
+
+
+setTimeout(()=>{
+    container.style.backgroundColor = "darksalmon"
+    n10.style.fontSize = "150px"
+    n10.innerHTML = "10"
+    setTimeout(()=>{
+        n10.innerHTML = " "
+    },500)
+},1000)
+
+setTimeout(()=>{
+    container.style.backgroundColor = "darkseagreen"
+    n10.style.fontSize = "150px"
+    n10.innerHTML = "9"
+    setTimeout(()=>{
+        n10.innerHTML = " "
+    },500)
+},2000)
+
+setTimeout(()=>{
+    container.style.backgroundColor = "gold"
+    n10.style.fontSize = "150px"
+    n10.innerHTML = "8"
+    setTimeout(()=>{
+        n10.innerHTML = " "
+    },500)
+},3000)
+
+setTimeout(()=>{
+    container.style.backgroundColor = "gray"
+    n10.style.fontSize = "150px"
+    n10.innerHTML = "7"
+    setTimeout(()=>{
+        n10.innerHTML = " "
+    },500)
+},4000)
+
+setTimeout(()=>{
+    container.style.backgroundColor = "greenyellow"
+    n10.style.fontSize = "150px"
+    n10.innerHTML = "6"
+    setTimeout(()=>{
+        n10.innerHTML = " "
+    },500)
+},5000)
+
+setTimeout(()=>{
+    container.style.backgroundColor = "darkorchid"
+    n10.style.fontSize = "150px"
+    n10.innerHTML = "5"
+    setTimeout(()=>{
+        n10.innerHTML = " "
+    },500)
+},6000)
+
+setTimeout(()=>{
+    container.style.backgroundColor = "dodgerblue"
+    n10.style.fontSize = "150px"
+    n10.innerHTML = "4"
+    setTimeout(()=>{
+        n10.innerHTML = " "
+    },500)
+},7000)
+
+setTimeout(()=>{
+    container.style.backgroundColor = "darkslateblue"
+    n10.style.fontSize = "150px"
+    n10.innerHTML = "3"
+    setTimeout(()=>{
+        n10.innerHTML = " "
+    },500)
+},8000)
+
+setTimeout(()=>{
+    container.style.backgroundColor = "darkgoldenrod"
+    n10.style.fontSize = "150px"
+    n10.innerHTML = "2"
+    setTimeout(()=>{
+        n10.innerHTML = " "
+    },500)
+},9000)
+
+setTimeout(()=>{
+    container.style.backgroundColor = "darkgray"
+    n10.style.fontSize = "150px"
+    n10.innerHTML = "1"
+    setTimeout(()=>{
+        n10.innerHTML = " "
+    },500)
+},10000)
+setTimeout(()=>{
+    n10.style.color = "red"
+    container.style.backgroundColor = "aqua"
+    n10.style.fontSize = "50px"
+    n10.innerHTML = "HAPPY INDEPENDENCE DAY"
+},11000)
+
+
